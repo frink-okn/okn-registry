6c04a5dc898c804e32b3029dfa3d660eb9c58dd0
Mahir Morshed <morshedm@renci.org>
2025-12-10 14:44:43 -0500

    adjust mkdocs.yml to include mermaid arguments

--- docs/javascripts/mermaid.mjs
+++
@@ -1,9 +0,0 @@
-import mermaid from 'https://unpkg.com/mermaid@10.4.0/dist/mermaid.esm.min.mjs';
-
-mermaid.initialize({
-    maxTextSize: 500000
-});
-
-// Important: necessary to make it visible to Material for MkDocs
-window.mermaid = mermaid;
-
